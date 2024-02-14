85b387043ad3cc7094f5a1eb4e9981d3dfe95cb5
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2024-02-14 20:28:55 +0100

    Added Home

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,FontAwesomeModule],
+  imports: [RouterOutlet, HeaderComponent, FontAwesomeModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
