bb4caf65924f9a5c6d62e5b3a41674d43b5f7530
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2024-02-17 16:45:14 +0100

    Update

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import {EducationComponent} from "./education/education.component";
 import {HomeComponent} from "./home/home.component";
 import {AllcomponentComponent} from "./allcomponent/allcomponent.component";
+import {SkillsComponent} from "./skills/skills.component";
 
 export const routes: Routes = [
 
   { path: '', title: '', component: AllcomponentComponent},
   { path: '', title: '', component: AllcomponentComponent},
-  { path: 'education', title: '', component: EducationComponent}
+  { path: 'education', title: '', component: EducationComponent},
+  { path: 'skills', title: '', component: SkillsComponent}
+
 
 ];
