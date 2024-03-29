1fcae575a0d8bdc84028e7f9bd95c9ea8267fa4e
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2024-03-29 14:07:07 +0100

    Update

--- portfolio/src/app/maincomponent/maincomponent.component.ts
+++ portfolio/src/app/maincomponent/maincomponent.component.ts
@@ -14,12 +14,13 @@ import {Router, RouterLink} from "@angular/router";
   templateUrl: './maincomponent.component.html',
   styleUrl: './maincomponent.component.scss'
 })
-export class MaincomponentComponent implements OnInit{
+export class MaincomponentComponent implements OnInit {
   title = 'portfolio';
   showMessage = true;
 
 
-  constructor(private router: Router, private elementRef: ElementRef) {}
+  constructor(private router: Router, private elementRef: ElementRef) {
+  }
 
   ngOnInit() {
     setInterval(() => {
